3a0d5208e2e62e337a15443fe5d9b910289e862f
Balamurugan Muthu <muruganmscit@gmail.com>
2019-08-28 11:30:58 -0700

    Create BallResponse.java
    Response to send back to client when an event happens

---
+++ server/src/main/java/com/olikaanoli/scoring/model/BallResponse.java
@@ -0,0 +1,15 @@
+package com.olikaanoli.scoring.model;
+
+import lombok.Data;
+
+@Data
+public class BallResponse {
+
+    //private int matchId;
+
+    private int total;
+
+    private int wickets;
+
+    private int extras;
+}
