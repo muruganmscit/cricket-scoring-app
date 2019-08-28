9372ae6d5bb8cffdee0a463042619894aafbdfee
Balamurugan Muthu <muruganmscit@gmail.com>
2019-08-28 11:45:47 -0700

    Update App.js
    Updated to added the new routing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import './App.css';
-
-import ListTeams from "./components/teams/ListTeams"
+import { Switch, Route } from 'react-router-dom'
+import CreateDetails from "./components/CreateDetails"
+import ShowDetails from "./components/ShowDetails"
 
 function App() {
   return (
-    <div className="App container">
-      <ListTeams />
+    <div className="App">
+      <div className="ph3 pv1 background-gray">
+        <Route exact path="/" component={CreateDetails} />
+        <Switch>
+          <Route exact path="/create/:id" component={CreateDetails} />
+          <Route exact path="/show/:id" component={ShowDetails} />
+        </Switch>
+      </div>
     </div>
   );
 }
